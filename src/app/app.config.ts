057fb339f3f51b50a4d7cd83c20bdab6b104f930
Naomi Coronel <151475890+Naomixcode@users.noreply.github.com>
2024-11-17 23:19:49 -0500

    add

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,34 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { authInterceptor } from './services/auth.interceptor';
 
+export function tokenGetter() {
+  return sessionStorage.getItem('token');
+}
 export const appConfig: ApplicationConfig = {
-  providers: [  provideZoneChangeDetection({ eventCoalescing: true }),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch()), provideAnimationsAsync()]
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])), // Se añade el interceptor aquí
+    provideAnimationsAsync(), provideCharts(withDefaultRegisterables()),
+    provideNativeDateAdapter(),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: ['localhost:8082'],
+          disallowedRoutes: ['http://localhost:8082/login/forget'],
+        },
+      })
+    )
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,96 +1,171 @@
 import { Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
-import { LoginComponent } from './components/Login/login.component';
-import { UserComponent } from './components/User/user.component';
-import { ListaruserComponent } from './components/User/listaruser/listaruser.component';
-import { CrearUserComponent } from './components/User/creaedita/creaeditauser.component';
-import { RoleComponent } from './components/Role/role.component';
-import { ListarRoleComponent } from './components/Role/listarrole/listarrole.component';
-import { CrearRoleComponent } from './components/Role/creaedita/creaeditarole.component';
-import { ProyectoComponent } from './components/Proyecto/proyecto.component';
-import { ListarProyectoComponent } from './components/Proyecto/listarproyecto/listarproyecto.component';
-import { CrearProyectoComponent } from './components/Proyecto/creaedita/creaeditaproyecto.component';
-import { PlanoComponent } from './components/Plano/plano.component';
-import { ListarPlanoComponent } from './components/Plano/listarplano/listarplano.component';
-import { CrearPlanoComponent } from './components/Plano/creaedita/creaeditaplano.component';
-import { MaterialComponent } from './components/material/material.component';
-import { ListarmaterialComponent } from './components/material/listarmaterial/listarmaterial.component';
-import { CreaeditamaterialComponent } from './components/material/creaeditamaterial/creaeditamaterial.component';
-import { PermisoComponent } from './components/permiso/permiso.component';
-import { ListarpermisoComponent } from './components/permiso/listarpermiso/listarpermiso.component';
-import { CreaeditapermisoComponent } from './components/permiso/creaeditapermiso/creaeditapermiso.component';
+import { HomeComponent } from './components/home/home.component';
+import { seguridadGuard } from './guard/seguridad.guard';
+import { LoginComponent } from './components/login/login.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { ListarUsuarioComponent } from './components/usuario/listarusuario/listarusuario.component';
+import { CreaEditaUsuarioComponent } from './components/usuario/creaeditausuario/creaeditausuario.component';
+import { RolComponent } from './components/rol/rol.component';
+import { ListarRolComponent } from './components/rol/listarrol/listarrol.component';
 import { NotificacionComponent } from './components/notificacion/notificacion.component';
-import { ListarnotificacionComponent } from './components/notificacion/listarnotificacion/listarnotificacion.component';
-import { CreaeditanotificacionComponent } from './components/notificacion/creaeditanotificacion/creaeditanotificacion.component';
-import { EvaluacionComponent } from './components/evaluacion/evaluacion.component';
-import { ListarEvaluacionComponent } from './components/evaluacion/listar-evaluacion/listar-evaluacion.component';
-import { CreaeditaEvalaucionComponent } from './components/evaluacion/creaedita-evalaucion/creaedita-evalaucion.component';
-import { PrototipoComponent } from './components/prototipo/prototipo.component';
-import { ListarPrototipoComponent } from './components/prototipo/listar-prototipo/listar-prototipo.component';
-import { CreaeditaPrototipoComponent } from './components/prototipo/creaedita-prototipo/creaedita-prototipo.component';
-import { ReporteComponent } from './components/reporte/reporte.component';
-import { ReporteproyectoporestadoComponent } from './components/reporte/reporteproyectoporestado/reporteproyectoporestado.component';
-import { ReporteterrenosporproyectoComponent } from './components/reporte/reporteterrenosporproyecto/reporteterrenosporproyecto.component';
+import { ListarNotificacionComponent } from './components/notificacion/listarnotificacion/listarnotificacion.component';
+import { CreaEditaNotificacionComponent } from './components/notificacion/creaeditanotificacion/creaeditanotificacion.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { ListarProyectoComponent } from './components/proyecto/listarproyecto/listarproyecto.component';
+import { CreaEditaProyectoComponent } from './components/proyecto/creaeditaproyecto/creaeditaproyecto.component';
+import { PermisoComponent } from './components/permiso/permiso.component';
+import { ListarPermisoComponent } from './components/permiso/listarpermiso/listarpermiso.component';
+import { CreaEditaPermisoComponent } from './components/permiso/creaeditapermiso/creaeditapermiso.component';
 import { ComentarioComponent } from './components/comentario/comentario.component';
-import { ListarcomentarioComponent } from './components/comentario/listarcomentario/listarcomentario.component';
-import { CreaeditacomentarioComponent } from './components/comentario/creaeditacomentario/creaeditacomentario.component';
-
+import { ListarComentarioComponent } from './components/comentario/listarcomentario/listarcomentario.component';
+import { CreaEditaComentarioComponent } from './components/comentario/creaeditacomentario/creaeditacomentario.component';
+import { EvaluacionComponent } from './components/evaluacion/evaluacion.component';
+import { ListarEvaluacionComponent } from './components/evaluacion/listarevaluacion/listarevaluacion.component';
+import { CreaEditaEvaluacionComponent } from './components/evaluacion/creaeditaevaluacion/creaeditaevaluacion.component';
+import { TerrenoComponent } from './components/terreno/terreno.component';
+import { ListarTerrenoComponent } from './components/terreno/listarterreno/listarterreno.component';
+import { CreaEditaTerrenoComponent } from './components/terreno/creaeditaterreno/creaeditaterreno.component';
+import { PlanoComponent } from './components/plano/plano.component';
+import { ListarPlanoComponent } from './components/plano/listarplano/listarplano.component';
+import { CreaEditaPlanoComponent } from './components/plano/creaeditaplano/creaeditaplano.component';
+import { PrototipoComponent } from './components/prototipo/prototipo.component';
+import { ListarPrototipoComponent } from './components/prototipo/listarprototipo/listarprototipo.component';
+import { CreaEditaPrototipoComponent } from './components/prototipo/creaeditaprototipo/creaeditaprototipo.component';
+import { PrototipoMaterialComponent } from './components/prototipomaterial/prototipomaterial.component';
+import { ListarPrototipoMaterialComponent } from './components/prototipomaterial/listarprototipomaterial/listarprototipomaterial.component';
+import { MaterialComponent } from './components/material/material.component';
+import { ListarMaterialComponent } from './components/material/listarmaterial/listarmaterial.component';
+import { CreaEditaPrototipoMaterialComponent } from './components/prototipomaterial/creaeditaprototipomaterial/creaeditaprototipomaterial.component';
+import { CreaEditaMaterialComponent } from './components/material/creaeditamaterial/creaeditamaterial.component';
+import { ComentariosPorUsuarioComponent } from './components/reportes/comentario-por-usuario/comentarios-por-usuario.component';
+import { ProyectosPorUsuarioComponent } from './components/reportes/proyecto-por-usuario/proyectos-por-usuario.component';
+import { ProyectosPorEstadoComponent } from './components/reportes/proyectos-por-estado/proyectos-por-estado.component';
+import { TerrenosPorProyectoComponent } from './components/reportes/terrenos-por-proyecto/terrenos-por-proyecto.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'usuarios', component: UserComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListaruserComponent },
-        { path: 'crear', component: CrearUserComponent },
-        { path: 'editar/:id', component: CrearUserComponent }  // Add this line for editing
-    ]},
-    { path: 'rol', component: RoleComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListarRoleComponent },
-        { path: 'crear', component: CrearRoleComponent },
-        { path: 'editar/:id', component: CrearRoleComponent }
-    ]},
-    { path: 'proyectos', component: ProyectoComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListarProyectoComponent },
-        { path: 'crear', component: CrearProyectoComponent },
-        { path: 'editar/:id', component: CrearProyectoComponent }  // Add this line for editing
-    ]},
-    { path: 'planos', component: PlanoComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListarPlanoComponent },
-        { path: 'crear', component: CrearPlanoComponent },
-        { path: 'editar/:id', component: CrearPlanoComponent }  // Add this line for editing
-    ]},
-    { path: 'materiales', component: MaterialComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListarmaterialComponent },
-        { path: 'crear', component: CreaeditamaterialComponent },
-        { path: 'editar/:id', component: CreaeditamaterialComponent }  // Add this line for editing
-    ]},
-    { path: 'permisos', component: PermisoComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListarpermisoComponent },
-        { path: 'crear', component: CreaeditapermisoComponent },
-        { path: 'editar/:id', component: CreaeditapermisoComponent }  // Add this line for editing
-    ]},
-    { path: 'notificaciones', component: NotificacionComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListarnotificacionComponent },
-        { path: 'crear', component: CreaeditanotificacionComponent },
-        { path: 'editar/:id', component: CreaeditanotificacionComponent }  // Add this line for editing
-    ]},
-    { path: 'evaluaciones', component: EvaluacionComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListarEvaluacionComponent },
-        { path: 'crear', component: CreaeditaEvalaucionComponent },
-        { path: 'editar/:id', component: CreaeditaEvalaucionComponent }  // Add this line for editing
-    ]},
-    { path: 'prototipos', component: PrototipoComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListarPrototipoComponent },
-        { path: 'crear', component: CreaeditaPrototipoComponent },
-        { path: 'editar/:id', component: CreaeditaPrototipoComponent }  // Add this line for editing
-    ]},
-    { path: 'comentarios', component: ComentarioComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: ListarcomentarioComponent },
-        { path: 'crear', component: CreaeditacomentarioComponent },
-        { path: 'editar/:id', component: CreaeditacomentarioComponent }  // Add this line for editing
-    ]},
-    { path: 'reportes', component: ReporteComponent, canActivate: [AuthGuard], children: [
-        { path: 'proyectosporestado', component: ReporteproyectoporestadoComponent }, 
-        { path: 'terrenosporproyecto', component: ReporteterrenosporproyectoComponent },// Add this line for editing
-    ]},
-    { path: '**', redirectTo: 'login' }
-];
+    {
+      path: '',
+      canActivate: [seguridadGuard],
+      children: [
+        { path: 'home', component: HomeComponent },
+        {
+          path: 'usuarios',
+          component: UsuarioComponent,
+          children: [
+            { path: '', component: ListarUsuarioComponent },
+            { path: 'nuevo', component: CreaEditaUsuarioComponent },
+            { path: 'editar/:id', component: CreaEditaUsuarioComponent }
+          ]
+        },
+        {
+          path: 'rol',
+          component: RolComponent, // Componente principal de roles
+          children: [
+            { path: '', component: ListarRolComponent },
+          ]
+        },
+        {
+          path: 'notificaciones',
+          component: NotificacionComponent,
+          children: [
+            { path: '', component: ListarNotificacionComponent },
+            { path: 'nuevo', component: CreaEditaNotificacionComponent },
+            { path: 'editar/:id', component: CreaEditaNotificacionComponent }
+          ]
+        },
+        {
+          path: 'proyectos',
+          component: ProyectoComponent,
+          children: [
+            { path: '', component: ListarProyectoComponent },
+            { path: 'nuevo', component: CreaEditaProyectoComponent },
+            { path: 'editar/:id', component: CreaEditaProyectoComponent }
+          ]
+        },
+        {
+          path: 'permisos',
+          component: PermisoComponent,
+          children: [
+            { path: '', component: ListarPermisoComponent },
+            { path: 'nuevo', component: CreaEditaPermisoComponent },
+            { path: 'editar/:id', component: CreaEditaPermisoComponent }
+          ]
+        },
+        {
+          path: 'comentarios',
+          component: ComentarioComponent,
+          children: [
+            { path: '', component: ListarComentarioComponent },
+            { path: 'nuevo', component: CreaEditaComentarioComponent },
+            { path: 'editar/:id', component: CreaEditaComentarioComponent }
+          ]
+        },
+        {
+          path: 'evaluaciones',
+          component: EvaluacionComponent,
+          children: [
+            { path: '', component: ListarEvaluacionComponent },
+            { path: 'nuevo', component: CreaEditaEvaluacionComponent },
+            { path: 'editar/:id', component: CreaEditaEvaluacionComponent }
+          ]
+        },
+        {
+          path: 'terrenos',
+          component: TerrenoComponent,
+          children: [
+            { path: '', component: ListarTerrenoComponent },
+            { path: 'nuevo', component: CreaEditaTerrenoComponent },
+            { path: 'editar/:id', component: CreaEditaTerrenoComponent }
+          ]
+        },
+        {
+          path: 'planos',
+          component: PlanoComponent,
+          children: [
+            { path: '', component: ListarPlanoComponent },
+            { path: 'nuevo', component: CreaEditaPlanoComponent },
+            { path: 'editar/:id', component: CreaEditaPlanoComponent }
+          ]
+        },
+        {
+          path: 'prototipos',
+          component: PrototipoComponent,
+          children: [
+            { path: '', component: ListarPrototipoComponent },
+            { path: 'nuevo', component: CreaEditaPrototipoComponent },
+            { path: 'editar/:id', component: CreaEditaPrototipoComponent }
+          ]
+        },
+        {
+          path: 'prototipos-material',
+          component: PrototipoMaterialComponent,
+          children: [
+            { path: '', component: ListarPrototipoMaterialComponent },
+            { path: 'nuevo', component: CreaEditaPrototipoMaterialComponent },
+            { path: 'editar/:id', component: CreaEditaPrototipoMaterialComponent }
+          ]
+        },
+        {
+          path: 'materiales',
+          component: MaterialComponent,
+          children: [
+            { path: '', component: ListarMaterialComponent },
+            { path: 'nuevo', component: CreaEditaMaterialComponent },
+            { path: 'editar/:id', component: CreaEditaMaterialComponent }
+          ]
+        },
+        {
+          path: 'reportes',
+          children: [
+            { path: 'comentarios-por-usuario', component: ComentariosPorUsuarioComponent },
+            { path: 'proyectos-por-usuario', component: ProyectosPorUsuarioComponent },
+            { path: 'proyectos-por-estado', component: ProyectosPorEstadoComponent },
+            { path: 'terrenos-por-proyecto', component: TerrenosPorProyectoComponent }
+          ]
+        },
+        { path: '', redirectTo: '/home', pathMatch: 'full' }
+      ]
+    },
+  ];
+  
