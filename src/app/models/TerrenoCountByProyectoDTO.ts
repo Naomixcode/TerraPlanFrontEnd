1e2d00e67a4c0cef2e12f9171f3a33e250335c9e
Naomi Coronel <151475890+Naomixcode@users.noreply.github.com>
2024-11-19 18:42:18 -0500

    aaddd

--- src/app/models/TerrenoCountByProyectoDTO.ts
+++ src/app/models/TerrenoCountByProyectoDTO.ts
@@ -1,11 +1,5 @@
-import { Rol } from "./Rol";
-import { Notificacion } from "./Notificacion";
-
-export class Usuario {
-    idUsuario: number = 0;
-    nombreCompleto: string = '';
-    contrasenia: string = '';
-    enabled: boolean = false;
-    roles: Rol[] = [];
-    notificaciones: Notificacion[] = [];
-  }
+export interface TerrenoCountByProyectoDTO {
+  proyectoId: number;
+  proyectoNombre: string;
+  numTerrenos: number;
+}
--- src/app/models/Usuario.ts
+++ src/app/models/Usuario.ts
@@ -1,31 +1,11 @@
-import { Role } from './Rol'; // Asegúrate de que la ruta sea correcta
-import { Proyecto } from './Proyecto'; // Ajusta según la ruta
-import { Comentario } from './Comentario'; // Ajusta según la ruta
+import { Rol } from "./Rol";
+import { Notificacion } from "./Notificacion";
 
-export class User {
-    idUsuario: number = 0; // ID del usuario
-    nombreCompleto: string = ""; // Nombre completo del usuario
-    contrasenia: string = ""; // Contraseña (considera manejarla de forma segura)
-    enabled: boolean = false; // Estado de habilitación
-    proyectos: Proyecto[] = []; // Lista de proyectos asociados al usuario
-    comentarios: Comentario[] = []; // Lista de comentarios asociados al usuario
-    roles: Role[] = []; // Arreglo de roles asociados al usuario
-
-    constructor(
-        idUsuario: number = 0,
-        nombreCompleto: string = "",
-        contrasenia: string = "",
-        enabled: boolean = false,
-        proyectos: Proyecto[] = [],
-        comentarios: Comentario[] = [],
-        roles: Role[] = []
-    ) {
-        this.idUsuario = idUsuario;
-        this.nombreCompleto = nombreCompleto;
-        this.contrasenia = contrasenia;
-        this.enabled = enabled;
-        this.proyectos = proyectos;
-        this.comentarios = comentarios;
-        this.roles = roles;
-    }
-}
+export class Usuario {
+    idUsuario: number = 0;
+    nombreCompleto: string = '';
+    contrasenia: string = '';
+    enabled: boolean = false;
+    roles: Rol[] = [];
+    notificaciones: Notificacion[] = [];
+  }
